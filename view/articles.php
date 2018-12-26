50a1d18e5023bd241c55212a8620de55a04641d4
jj861119 <jj861119@gmail.com>
2018-12-26 15:18:05 +0800

    改header

---
+++ view/articles.php
@@ -0,0 +1,65 @@
+<div class="content-wrapper">
+    <!-- article -->
+    <div class="box box-success">
+        <div class="box-header">
+            <h3 class="box-title">Article</h3>
+        </div>
+        <div class="box-body chat" id="chat-box">
+            <div class="item">
+                <img src="../img/avatar5.png" alt="user image" class="online">
+                <p class="message">
+                    <a href="#" class="name">
+                        Mike Doe
+                    </a>
+                    I would like to meet you to discuss the latest news about
+                    the arrival of the new theme. They say it is going to be one the
+                    best themes on the market
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- /article -->
+    <!-- article -->
+    <div class="box box-success">
+        <div class="box-header">
+            <h3 class="box-title">Article 2</h3>
+        </div>
+        <div class="box-body chat" id="chat-box">
+            <div class="item">
+                <img src="../img/avatar5.png" alt="user image" class="online">
+                <p class="message">
+                    <a href="#" class="name">
+                        Mike Doe
+                    </a>
+                    I would like to meet you to discuss the latest news about
+                    the arrival of the new theme. They say it is going to be one the
+                    best themes on the market
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- /article -->
+
+    <!-- article -->
+    <div class="box box-success">
+        <div class="box-header">
+            <h3 class="box-title">Article 3</h3>
+        </div>
+        <div class="box-body chat" id="chat-box">
+            <div class="item">
+                <img src="../img/avatar5.png" alt="user image" class="online">
+                <p class="message">
+                    <a href="#" class="name">
+                        Mike Doe
+                    </a>
+                    I would like to meet you to discuss the latest news about
+                    the arrival of the new theme. They say it is going to be one the
+                    best themes on the market
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- /article -->
+
+
+</div>
