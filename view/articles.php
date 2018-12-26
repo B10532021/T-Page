7057c9706c4b6335c4299de1b5b70a60956354ee
jj861119 <jj861119@gmail.com>
2018-12-26 15:49:48 +0800

    文章頁面完成

--- view/articles.php
+++ view/articles.php
@@ -1,4 +1,8 @@
-<div class="content-wrapper">
+<div class="content-wrapper" style="min-height:100%;height: 100%; overflow: scroll">
+    <div style="width: 70%;display: inline-block;">
+        <div class="callout callout-info">
+            <h4>四電資一</h4>
+        </div>
     <!-- article -->
     <div class="box box-success">
         <div class="box-header">
@@ -60,6 +64,51 @@
         </div>
     </div>
     <!-- /article -->
+        <!-- article -->
+        <div class="box box-success">
+            <div class="box-header">
+                <h3 class="box-title">Article 3</h3>
+            </div>
+            <div class="box-body chat" id="chat-box">
+                <div class="item">
+                    <img src="../img/avatar5.png" alt="user image" class="online">
+                    <p class="message">
+                        <a href="#" class="name">
+                            Mike Doe
+                        </a>
+                        I would like to meet you to discuss the latest news about
+                        the arrival of the new theme. They say it is going to be one the
+                        best themes on the market
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- /article -->
+        <!-- article -->
+        <div class="box box-success">
+            <div class="box-header">
+                <h3 class="box-title">Article 3</h3>
+            </div>
+            <div class="box-body chat" id="chat-box">
+                <div class="item">
+                    <img src="../img/avatar5.png" alt="user image" class="online">
+                    <p class="message">
+                        <a href="#" class="name">
+                            Mike Doe
+                        </a>
+                        I would like to meet you to discuss the latest news about
+                        the arrival of the new theme. They say it is going to be one the
+                        best themes on the market
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- /article -->
+    </div>
 
-
+    <div style="position: relative;height: 100%;width: 30%;float: right;">
+        <a href="https://store.playstation.com/zh-hant-tw/grid/STORE-MSF86012-HS_GAME_ASIA/1?emcid=tw-cht_ad_holiday_bahamut">
+            <img src="../img/ad.jpg">
+        </a>
+    </div>
 </div>
