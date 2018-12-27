f445d6df1658fe90d34a3a21deab6bca8fa16363
B10532021 <b10532021@gapps.ntust.edu.tw>
2018-12-27 18:00:58 +0800

    view1 profile暫定樣子完成

--- view1/view/profile.php
+++ view1/view/profile.php
@@ -13,7 +13,110 @@
 
     <!-- Main content -->
     <section class="content">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="gray_tshirt">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="home">
+                            <div class="single_p_image">
+                                <a
+                                        href="/assets/images/single-large.jpg"
+                                        data-lightbox="image-1"
+                                        data-title="My caption"
+                                ><img src="images/product-plus.png" alt=""/></a>
+                                <img
+                                        id="zoom_02"
+                                        src="../img/angel.jpg"
+                                        data-zoom-image="/assets/images/single-large.jpg"
+                                        alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <div class="product_detail_heading">
+                            <div><h3>個人簡介</h3>
+                                <div class="detail_heading_left">
+                                    <h4>姓名：大美人</h4>
+                                </div>
+                            </div>
+                            <div class="product_detail_heading">
+                                <div class="detail_heading_left">
+                                    <div class="detail_heading_left">
+                                        <h4>學校：國立台灣科技大學企管系</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="product_detail_heading">
+                                <div class="detail_heading_left">
+                                    <div class="detail_heading_left">
+                                        <h4>性別：女</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="product_detail_heading">
+                                <div class="detail_heading_left">
+                                    <div class="detail_heading_left">
+                                        <h4>生日：1998/01/01</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="product_detail_heading">
+                                <div class="detail_heading_left">
+                                    <div class="detail_heading_left">
+                                        <h4>興趣：打撞球、讀小說、聽音樂</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="product_detail_heading">
+                                <div class="detail_heading_left">
+                                    <div class="detail_heading_left">
+                                        <h4>參加的社團：跆拳道社</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h3>已發表的文章：</h3>
+        <hr class="style-five"/>
+        <div class="box box-success">
+            <div class="box-body chat" id="chat-box">
+                <div class="item">
+                    <img src="../img/avatar5.png" alt="user image" class="online">
+                    <p class="message">
+                        <a href="#" class="name">
+                            Mike Doe
+                        </a>
+                        I would like to meet you to discuss the latest news about
+                        the arrival of the new theme. They say it is going to be one the
+                        best themes on the market
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- /article -->
+        <!-- article -->
+        <div class="box box-success">
+            <div class="box-body chat" id="chat-box">
+                <div class="item">
+                    <img src="../img/avatar5.png" alt="user image" class="online">
+                    <p class="message">
+                        <a href="#" class="name">
+                            Mike Doe
+                        </a>
+                        I would like to meet you to discuss the latest news about
+                        the arrival of the new theme. They say it is going to be one the
+                        best themes on the market
+                    </p>
+                </div>
+            </div>
+        </div>
     </section>
-    <!-- /.content -->
+
 </div>
