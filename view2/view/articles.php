86df4ddf79d0df2fb4115778fa876949f100c7eb
jj861119 <jj861119@gmail.com>
2018-12-27 19:02:08 +0800

    view2文章部分

---
+++ view2/view/articles.php
@@ -0,0 +1,83 @@
+
+	<!-- Main -->
+		<div id="main">
+			<div class="container">
+				<header>
+					<h2>四電資一</h2>
+				</header>
+
+                <div class="divider">&nbsp;</div>
+
+                <!--       文章         -->
+				<div class="row">
+					<div class="3u">
+						<section>
+							<p>Quisque dictum. Pellentesque viverra  enim. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
+							<a href="#" class="button">Read More</a>
+						</section>
+					</div>
+					<div class="3u">
+						<section>
+							<p>Pellentesque viverra  enim. Tristique ante ut risus. Quisque dictum. Integer sagittis convallis elementum.</p>
+							<a href="#" class="button">Read More</a>
+						</section>
+					</div>
+					<div class="3u">
+						<section>
+							<p>Pellentesque viverra  enim. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
+							<a href="#" class="button">Read More</a>
+						</section>
+					</div>
+					<div class="3u">
+						<section>
+							<p>Tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis elementum.</p>
+							<a href="#" class="button">Read More</a>
+						</section>
+					</div>
+				</div>
+                <!--       /文章         -->
+
+                <!--       文章         -->
+                <div class="row">
+                    <div class="3u">
+                        <section>
+                            <p>Quisque dictum. Pellentesque viverra  enim. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
+                            <a href="#" class="button">Read More</a>
+                        </section>
+                    </div>
+                    <div class="3u">
+                        <section>
+                            <p>Pellentesque viverra  enim. Tristique ante ut risus. Quisque dictum. Integer sagittis convallis elementum.</p>
+                            <a href="#" class="button">Read More</a>
+                        </section>
+                    </div>
+                    <div class="3u">
+                        <section>
+                            <p>Pellentesque viverra  enim. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
+                            <a href="#" class="button">Read More</a>
+                        </section>
+                    </div>
+                    <div class="3u">
+                        <section>
+                            <p>Tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis elementum.</p>
+                            <a href="#" class="button">Read More</a>
+                        </section>
+                    </div>
+                </div>
+                <!--       /文章         -->
+
+                <div class="row">
+                    <!-- Content -->
+                    <div class="12u skel-cell-important" >
+                        <section id="content" >
+                            <p><a href="https://www.momoshop.com.tw/edm/cmmedm.jsp?lpn=O0YtsjW2Bo6&n=1&mdiv=1000000000-bt_0_240_01-bt_0_240_01_P1_2_e1&ctype=B" class="image full">
+                                    <img src="../images/banner.JPG" alt="" style="height: 100%">
+                                </a></p>
+                        </section>
+                    </div>
+                    <!-- /Content -->
+                </div>
+			
+			</div>
+		</div>
+	<!-- Main -->
---
+++ view2/view/card.php
@@ -0,0 +1,71 @@
+
+	<!-- Main -->
+		<div id="main">
+			<div class="container">
+				<header>
+					<h2>四電資一</h2>
+				</header>
+
+                <div class="divider">&nbsp;</div>
+
+                <!--       文章         -->
+				<div class="row">
+					<div class="3u">
+						<section>
+							<p>Quisque dictum. Pellentesque viverra  enim. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
+							<a href="#" class="button">Read More</a>
+						</section>
+					</div>
+					<div class="3u">
+						<section>
+							<p>Pellentesque viverra  enim. Tristique ante ut risus. Quisque dictum. Integer sagittis convallis elementum.</p>
+							<a href="#" class="button">Read More</a>
+						</section>
+					</div>
+					<div class="3u">
+						<section>
+							<p>Pellentesque viverra  enim. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
+							<a href="#" class="button">Read More</a>
+						</section>
+					</div>
+					<div class="3u">
+						<section>
+							<p>Tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis elementum.</p>
+							<a href="#" class="button">Read More</a>
+						</section>
+					</div>
+				</div>
+                <!--       /文章         -->
+
+                <!--       文章         -->
+                <div class="row">
+                    <div class="3u">
+                        <section>
+                            <p>Quisque dictum. Pellentesque viverra  enim. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
+                            <a href="#" class="button">Read More</a>
+                        </section>
+                    </div>
+                    <div class="3u">
+                        <section>
+                            <p>Pellentesque viverra  enim. Tristique ante ut risus. Quisque dictum. Integer sagittis convallis elementum.</p>
+                            <a href="#" class="button">Read More</a>
+                        </section>
+                    </div>
+                    <div class="3u">
+                        <section>
+                            <p>Pellentesque viverra  enim. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
+                            <a href="#" class="button">Read More</a>
+                        </section>
+                    </div>
+                    <div class="3u">
+                        <section>
+                            <p>Tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis elementum.</p>
+                            <a href="#" class="button">Read More</a>
+                        </section>
+                    </div>
+                </div>
+                <!--       /文章         -->
+			
+			</div>
+		</div>
+	<!-- Main -->
