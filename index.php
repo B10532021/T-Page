819e35cc6d4d29965332fca1df2ffacd9a8e9e75
jj861119 <jj861119@gmail.com>
2018-12-27 14:39:17 +0800

    加view2

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$page = "view1/view/articles.php";
+$title = "Card_Friend";
+
+if(isset($_GET["page"])) {
+    if($_GET["page"] == "articles") {
+        $page = "view1/view/articles.php";
+    }
+    if($_GET["page"] == "card") {
+        $page = "view1/view/card.php";
+    }
+    if($_GET["page"] == "profile") {
+    $page = "view1/view/profile.php";
+    }
+}
+
+include("view1/layouts/header.php");
+
+include("view1/layouts/sidebar.php");
+
+include($page);
+
+include("view1/layouts/footer.php");
+
+?>
