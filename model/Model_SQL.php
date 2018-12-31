2a9bc2efa5bde1183ad5df67c6c897fc1314a15a
B10532004 <lucky40303@gmail.com>
2018-12-31 16:17:36 +0800

    新增功能

--- model/Model_SQL.php
+++ model/Model_SQL.php
@@ -1,16 +1,19 @@
 <?php
 class Model_SQL
 {
-    protected $table;
-    protected $primary = 'id';
-    private $param = array();
-
+    private $articleID = 1;
+    private $userID = 1;
     private $state;
+
+    private $advertisementData = array();
     private $articleData = array();
     private $boardData = array();
+    private $cardFriendData = array();
+    private $familyData = array();
     private $messageData = array();
     private $userData = array();
 
+    //跟狀態有關
     public function getState()
     {
         return $this->state;
@@ -19,152 +22,219 @@ class Model_SQL
     {
         $this->state = $aState;
     }
-
-    public function addArticle($title, $board, $author, $content)
+    //跟廣告有關
+    public function addAdvertisement($title, $content)
     {
         $conn = mysqli_connect("localhost:33060", "root", "root");
-        mysqli_select_db($conn, "t_page");
+        mysqli_select_db($conn, "tpage");
         mysqli_query( $conn, "SET NAMES 'utf8");
-        $sql = "INSERT INTO articles(title, board, author, content) VALUES ('$title', '$board', '$author', '$content')";
+        $sql = "INSERT INTO advertisement(title, billboard) VALUES ('$title', '$content')";
         mysqli_query($conn, $sql);
         mysqli_close($conn);
     }
-    private function getArticleData($board)
+    private function getAdvertisementData($title)
     {
+        $this->advertisementData = array();
+        $conn = mysqli_connect("localhost:33060","root","root");
+        mysqli_select_db($conn, "tpage");
+        mysqli_query( $conn, "SET NAMES 'utf8'");
+        $sql = "select * from tpage.advertisement where title = '$title'";
+        $result = mysqli_query($conn, $sql);
+        while($advertisement = mysqli_fetch_row($result)) {
+            $this->advertisementData[] = $advertisement;
+        }
+    }
+    public function searchAdvertisement($title)
+    {
+        $this->getAdvertisementData($title);
+        return $this->advertisementData;
+    }
+    //跟文章有關
+    public function addArticle($title, $board, $author, $content)
+    {
+        $conn = mysqli_connect("localhost:33060", "root", "root");
+        mysqli_select_db($conn, "tpage");
+        mysqli_query( $conn, "SET NAMES 'utf8");
+        $sql = "INSERT INTO articles(title, board, author, content, articleID) VALUES ('$title', '$board', '$author', '$content', '$this->articleID')";
+        mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        $this->articleID++;
+    }
+    private function getArticleData($title)
+    {
+        $this->articleData = array();
         $conn = mysqli_connect("localhost:33060","root","root");
         //設定連線編碼
         mysqli_select_db($conn, "tpage");
         mysqli_query( $conn, "SET NAMES 'utf8'");
-        $sql = "select * from tpage.articles where board='$board'";
+        $sql = "select * from tpage.articles where title = '$title'";
         $result = mysqli_query($conn, $sql);
         while($article = mysqli_fetch_row($result)) {
             $this->articleData[] = $article;
         }
-
     }
-    public function searchArticle($board)
+    public function searchArticle($title)
     {
-        $this->getArticleData($board);
+        $this->getArticleData($title);
         return $this->articleData;
     }
-    public function addBoard($ECE, $TAA)
+    public function getArticleID($title)
     {
-        $conn = mysqli_connect("localhost:33060", "root", "root");
-        mysqli_select_db($conn, "t_page");
-        mysqli_query( $conn, "SET NAMES 'utf8");
-        if($ECE == "ECE1")
-        {
-            $sql = "INSERT INTO board(ECE1, ECE2, ECE3, ECE4) VALUES ('$TAA', , ,)";
-        }
-        else if($ECE == "ECE2")
-        {
-            $sql = "INSERT INTO board(ECE1, ECE2, ECE3, ECE4) VALUES ( ,'$TAA', , )";
-        }
-        else if($ECE == "ECE3")
-        {
-            $sql = "INSERT INTO board(ECE1, ECE2, ECE3, ECE4) VALUES ( , , '$TAA', )";
-        }
-        else if($ECE == "ECE4")
-        {
-            $sql = "INSERT INTO board(ECE1, ECE2, ECE3, ECE4) VALUES ( , , ,'$TAA')";
-        }
-        mysqli_query($conn, $sql);
-        mysqli_close($conn);
+        $this->searchArticle($title);
+        return $this->articleData[4];
     }
-    private function getBoardData()
+    //跟看板有關
+    private function getBoardData($board)
     {
+        $this->boardData = array();
         $conn = mysqli_connect("localhost:33060","root","root");
-        //設定連線編碼
         mysqli_query( $conn, "SET NAMES 'utf8'");
-        $sql = "select * from tpage.board";
+        $sql = "select * from tpage.articles where board = '$board'";
         $result = mysqli_query($conn, $sql);
-        $this->boardData = mysqli_fetch_row($result);
+        while($board = mysqli_fetch_row($result)) {
+            $this->boardData[] = $board;
+        }
     }
-    public function searchBoard()
+    public function searchBoard($board)
     {
-        $this->getBoardData();
+        $this->getBoardData($board);
         return $this->boardData;
     }
-    public function addMessage($name, $message, $board)
+    //跟卡友有關
+    public function addCardFriend()
+    {
+        return true;
+    }
+    public function becomeCardFriend($me, $friend)
     {
         $conn = mysqli_connect("localhost:33060", "root", "root");
-        mysqli_select_db($conn, "t_page");
+        mysqli_select_db($conn, "tpage");
         mysqli_query( $conn, "SET NAMES 'utf8");
-        $sql = "INSERT INTO message(name, message, board) VALUES ('$name', '$message', '$board')";
+        $sql = "INSERT INTO cardfriend(me, friend) VALUES ('$me', '$friend')";
+        $sql2 = "INSERT INTO cardfriend(me, friend) VALUES ('$friend', '$me')";
+        mysqli_query($conn, $sql);
+        mysqli_query($conn, $sql2);
+        mysqli_close($conn);
+    }
+    private function getCardFriendData($name)
+    {
+        $this->cardFriendData = array();
+        $conn = mysqli_connect("localhost:33060","root","root");
+        mysqli_query( $conn, "SET NAMES 'utf8'");
+        $sql = "select * from tpage.cardfriend where me = '$name'";
+        $result = mysqli_query($conn, $sql);
+        while($friend = mysqli_fetch_row($result)) {
+            $this->cardFriendData[] = $friend;
+        }
+    }
+    public function searchCardFriendData($name)
+    {
+        $this->getCardFriendData($name);
+        return $this->cardFriendData[1];
+    }
+    //跟家族有關
+    public function addFamily()
+    {
+        return true;
+    }
+    public function allowEnterFamily()
+    {
+        return true;
+    }
+    public function jointFamily($member, $familyID)
+    {
+        $conn = mysqli_connect("localhost:33060", "root", "root");
+        mysqli_select_db($conn, "tpage");
+        mysqli_query( $conn, "SET NAMES 'utf8");
+        $sql = "INSERT INTO family(member, familyID) VALUES ('$member', '$familyID')";
+        $this->searchUserData($member);
+        $sql2 = "UPDATE users SET family = '$familyID' where name = '$this->userData[0]' AND school = '$this->userData[1]' 
+        AND gender = '$this->userData[2]' AND birth = '$this->userData[3]' AND interests = '$this->userData[4]' AND clubs = '$this->userData[5]'
+        AND  userID = '$this->userData[7]' AND cardFriend  = '$this->userData[8]'";
+        mysqli_query($conn, $sql);
+        mysqli_query($conn, $sql2);
+        mysqli_close($conn);
+    }
+    private function getFamilyData($familyID)
+    {
+        $this->familyData = array();
+        $conn = mysqli_connect("localhost:33060","root","root");
+        mysqli_query( $conn, "SET NAMES 'utf8'");
+        $sql = "select * from tpage.family where familyID = '$familyID'";
+        $result = mysqli_query($conn, $sql);
+        while($family = mysqli_fetch_row($result)) {
+            $this->familyData[] = $family;
+        }
+    }
+    public function searchFamily($familyID)
+    {
+        $this->getFamilyData($familyID);
+        return $this->familyData[0];
+    }
+    //跟訊息有關
+    public function addMessage($name, $message, $article_ID)
+    {
+        $conn = mysqli_connect("localhost:33060", "root", "root");
+        mysqli_select_db($conn, "tpage");
+        mysqli_query( $conn, "SET NAMES 'utf8");
+        $sql = "INSERT INTO message(name, message, articleID) VALUES ('$name', '$message', '$article_ID')";
         mysqli_query($conn, $sql);
         mysqli_close($conn);
     }
-    private function getMessageData()
+    private function getMessageData($article_ID)
     {
+        $this->messageData = array();
         $conn = mysqli_connect("localhost:33060","root","root");
         //設定連線編碼
         mysqli_query( $conn, "SET NAMES 'utf8'");
-        $sql = "select * from tpage.message";
+        $sql = "select * from tpage.message where articleID = '$article_ID'";
         $result = mysqli_query($conn, $sql);
-        $this->messageData = mysqli_fetch_row($result);
+        while($message = mysqli_fetch_row($result)) {
+            $this->messageData[] = $message;
+        }
     }
-    public function searchMessage()
+    public function searchMessage($article_ID)
     {
-        $this->getMessageData();
+        $this->getMessageData($article_ID);
         return $this->messageData;
     }
+    //跟使用者有關
     public function addUser($name, $school, $gender, $birth, $interests, $clubs, $family)
     {
         $conn = mysqli_connect("localhost:33060", "root", "root");
-        mysqli_select_db($conn, "t_page");
+        mysqli_select_db($conn, "tpage");
         mysqli_query( $conn, "SET NAMES 'utf8");
-        $sql = "INSERT INTO users(name, school, gender, birth, interests, clubs, family) VALUES ('$name', '$school', '$gender', '$birth', '$interests', '$clubs', '$family')";
+        $sql = "INSERT INTO users(name, school, gender, birth, interests, clubs, family, userID) VALUES ('$name', '$school', '$gender', '$birth', '$interests', '$clubs', '$family', $this->userID)";
         mysqli_query($conn, $sql);
         mysqli_close($conn);
+        $this->userID++;
     }
-    private function getUserData()
+    private function getUserData($name = null)
     {
+        $this->userData = array();
         $conn = mysqli_connect("localhost:33060","root","root");
         //設定連線編碼
         mysqli_query( $conn, "SET NAMES 'utf8'");
-        $sql = "select * from tpage.users";
+        if($name == null)
+        {
+            $sql = "select * from tpage.users";
+        }
+        else{
+            $sql = "select * from tpage.users where name = '$name'";
+        }
         $result = mysqli_query($conn, $sql);
-        $this->userData = mysqli_fetch_row($result);
+        while($user = mysqli_fetch_row($result)){
+            $this->userData[] = $user;
+        }
     }
-    public function searchUser()
+    public function searchUserData($name)
     {
-        $this->getUserData();
+        $this->getUserData($name);
         return $this->userData;
     }
-
-    public function add($data)
+    public function getUserID($name)
     {
-        $sql = sprintf("insert into `%s `%s", $this->table, $this->formatInsert($data));
-        $sth = DB::pdo()->prepare($sql);
-        $sth = $this->formatParam($sth, $data);
-        $sth = $this->formatParam($sth, $this->param);
-        $sth->execute();
-
-        return $sth->rowCount();
-    }
-    private function formatInsert($data)
-    {
-        $fields = array();
-        $names = array();
-        foreach ($data as $key => $value) {
-
-            $fields[] = sprintf("`%s`", $key);
-            $names[] = sprintf(":%s", $key);
-        }
-
-        $field = implode(',', $fields);
-        $name = implode(', ', $names);
-
-        return sprintf("(%s) values (%s)", $field, $name);
-    }
-    public function formatParam(PDOStatement $sth, $params = array())
-    {
-        foreach ($params as $param => &$value) {
-
-            $param = is_int($param) ? $param + 1 : ':' . ltrim($param, ':');
-            $sth->bindParam($param, $value);
-        }
-        return $sth;
-
+        $this->searchUserData($name);
+        return $this->articleData[7];
     }
 }
