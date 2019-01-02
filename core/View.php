bda7d8bdfd9ae89e617d726cbaf1a51e71e066ce
B10532021 <b10532021@gapps.ntust.edu.tw>
2019-01-02 20:51:42 +0800

    Views

--- core/View.php
+++ core/View.php
@@ -7,14 +7,21 @@ class View
 {
     public static function render($view, $args = [])
     {
+        $views = 'Views1';
         extract($args, EXTR_SKIP);
 
-        $file = dirname(__DIR__) . "/app/Views/$view";  // relative to Core directory
+        require dirname(__DIR__) . "/app/$views/layouts/header.php";
+        require dirname(__DIR__) . "/app/$views/layouts/sidebar.php";
+
+
+        $file = dirname(__DIR__) . "/app/$views/view/$view";  // relative to Core directory
         if (is_readable($file)) {
             require $file;
         } else {
             echo 'no view';
         }
+
+        require dirname(__DIR__) . "/app/$views/layouts/footer.php";
     }
 
 }
